c204939219390efa4b49d5104c508c52dfcd0689
avinashbiradar <avinashbiradar179@gmail.com>
2020-12-12 05:16:30 +0530

    Welcome to AddressBook program

---
+++ AddressBookMainClass.java
@@ -0,0 +1,12 @@
+package com.Project.AddressBook;
+
+public class AddressBookMainClass {
+       public static void main(String[] args) {
+				System.out.println("Welcome to Address Book Program");
+     		}
+
+	 	
+  }
+
+
+	
